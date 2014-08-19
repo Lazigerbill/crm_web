76e004dac1e779c21e14cf93bcf1f89ec193a113
Lazigerbill <chaukbill@gmail.com>
2014-08-19 14:29:57 -0400

    Contacts.rb included

--- crm.rb
+++ crm.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
+require_relative 'Contact.rb'
 
 get '/' do
+	@crm_app_name="Bill\'s CRM"
 	erb :index
+end
+
+get '/contacts' do 
+	@contacts=[]
+  	@contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
+  	@contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
+  	@contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
+	erb :contacts
+end
+
+get '/contacts_new' do 
+	erb :contacts_new
 end
