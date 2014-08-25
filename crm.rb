2beebe73748090af9cd13a5c8089e7bb602918bf
Lazigerbill <chaukbill@gmail.com>
2014-08-25 16:24:04 -0400

    CRUD complete

--- crm.rb
+++ crm.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'data_mapper'
+require 'pry'
 
 DataMapper.setup(:default, "sqlite3:database.sqlite3") #this is to setup the database file .sqlite3 is the file extension
 
@@ -10,19 +11,11 @@ class Contact
 	property :last_name, String
 	property :email, String
 	property :note, String
-	# attr_accessor(:id, :first_name, :last_name, :email, :note)
-  	# def initialize(id=nil,first_name=nil, last_name=nil, email=nil, note=nil)
-   	#  	@id = id
-   	#  	@first_name = first_name
-   	#  	@last_name = last_name
-   	#  	@email = email
-   	#  	@note = note
+
 end
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
-
-
 
 get '/' do
 	erb :index
@@ -41,20 +34,45 @@ get '/photos' do
 	erb :photos
 end
 
-get '/contacts/new' do 
+get '/contacts_new' do 
 	erb :contacts_new
 end
 
-# post '/contacts' do
-#   puts params
-
-post '/contacts' do
+post '/contacts_new' do
 contact = Contact.create(
 	:first_name => params[:first_name],
 	:last_name => params[:last_name], 
 	:email => params[:email],
 	:note => params[:note])
-
 redirect '/contacts'
-
 end
+
+post '/search_results' do 
+	@search_results = 
+	Contact.all(:conditions =>{ :first_name.like => "%#{params[:first_name]}%"}) &
+	Contact.all(:conditions =>{ :last_name.like => "%#{params[:last_name]}%"})
+	erb :search_results
+end
+
+get '/contacts/:id' do 
+	@contact = Contact.get(params[:id])
+	erb :show_contact
+end
+
+put '/edit/:id' do
+	@contact = Contact.get(params[:id])
+	@contact.update(
+	:first_name => params[:first_name],
+	:last_name => params[:last_name], 
+	:email => params[:email],
+	:note => params[:note])
+redirect '/contacts'
+end
+
+get '/delete/:id' do 
+	@contact = Contact.get(params[:id])
+	@contact.destroy
+	redirect '/contacts'
+end
+
+
